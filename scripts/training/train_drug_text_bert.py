CrossAtt model fixed context attention mask

scripts/training/train_drug_text_bert.py
@@ -427,8 +427,7 @@ class CrossModalityBertClassifier(nn.Module):
                                        return_dict=True)['last_hidden_state']
         # molecule_cls_embeddings = torch.stack([elem[0, :] for elem in molecule_last_hidden_states])
         cross_attention_output = self.cross_attention_layer(input_tensor=text_last_hidden_states,
-                                                            ctx_tensor=molecule_last_hidden_states,
-                                                            ctx_att_mask=molecule_attention_mask)
+                                                            ctx_tensor=molecule_last_hidden_states,)
 
         proba = self.classifier(cross_attention_output)
         return proba