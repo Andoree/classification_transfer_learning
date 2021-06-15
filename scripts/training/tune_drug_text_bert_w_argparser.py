ba2621f5f15aa296bf2fc437dbd0f063df67f180
Andoree <and2900@inbox.ru>
2021-06-15 14:00:38 +0300

    Changed train fraction range

--- scripts/training/tune_drug_text_bert_w_argparser.py
+++ scripts/training/tune_drug_text_bert_w_argparser.py
@@ -753,7 +753,8 @@ def main():
     else:
         train_range = [train_size, ]
     if use_train_fracs:
-        train_fracs_list = [1.0]
+        train_fracs_list = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8,
+                            0.85, 0.9, 0.95, 1.0]
     else:
         train_fracs_list = [1.0]
 
